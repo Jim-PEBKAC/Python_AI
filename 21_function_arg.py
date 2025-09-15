951a248209fdb0e41c51fef97e3f156e86560d47
Jim <jimmyyy@nate.com>
2025-09-15 10:00:29 +0900

    함수 인자값(튜플로 받기)

--- 21_function_arg.py
+++ 21_function_arg.py
@@ -5,3 +5,13 @@ def plus(num=0):
 
 print(plus(5))  # 프린트 값 10
 print(plus())
+
+
+# 인자값(기본값)의 종류를 튜플(수정불가 List 형태)로만 받겠다 /별(*)표시를 앞에 붙이면
+def tuple_args(*numbers):
+    print(numbers)
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+print(tuple_args(1,2,3,4,5))
