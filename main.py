e0f0b2455e9fc09edd1236e592bc25b263001574
Jim <jimmyyy@nate.com>
2025-09-16 11:53:04 +0900

    패키지 불러오기를 통한 모듈 함수 사용 방법

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-from com
+# 모듈에 함수까지 전부 불러와서 사용하는 방법
+from com.py.oop.other_module import other_function
+other_function()
+
+# 모듈을 불러와서 별칭 설정 후 사용하는 방법
+from com.py.oop import other_module as mo
+mo.other_function()
