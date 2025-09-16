87af3ea46bb2f758a3d6c06e2f47aae6a6942a05
Jim <jimmyyy@nate.com>
2025-09-16 14:57:20 +0900

    부모자식(덮어쓰기+기존 내용 응용하기)

--- advance/10_override.py
+++ advance/10_override.py
@@ -3,16 +3,26 @@ class Car:
         print('시동이 걸린다.')
 
     def run(self):
-        print('차가 시속 50km로 달린다.')
+        print('차가 시속 50km 로 달린다.')
 
     def stop(self):
         print('차가 멈춘다.')
 
 class MyCar(Car):
-    def start(self):
-        print('시동이 켜진다')
 
-new = MyCar()
-new.start()
-new.run()
-new.stop()
+    turbo = False
+
+    def run(self): # 부모와 같은 메서드를 사용하면 override 로 인식된다.
+        if self.turbo == True:
+            print('차가 시속 200km 로 달린다.')
+        else:
+            super().run() #부모의 run 을 그대로 쓰겠다.
+
+
+mc = MyCar()
+mc.start()
+
+mc.run()
+mc.turbo = True
+mc.run()
+mc.stop()
